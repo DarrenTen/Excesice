b0ac0a618fa90849428e1d14188a02c385df8c22
DarrenTen <flytianxia1990@gmail.com>
2014-06-28 15:01:06 +0800

    添加winform的操作

--- PudongTraining/PudongTraining/Form1.cs
+++ PudongTraining/PudongTraining/Form1.cs
@@ -6,6 +6,8 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using PudongTraining.Utility;
+using System.Data.SqlClient;
 
 namespace PudongTraining
 {
@@ -14,6 +16,30 @@ namespace PudongTraining
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: 这行代码将数据加载到表“productDBDataSet.Customer”中。您可以根据需要移动或删除它。
+            this.customerTableAdapter.Fill(this.productDBDataSet.Customer);
+            // TODO: 这行代码将数据加载到表“productDBDataSet.vwUserOrder”中。您可以根据需要移动或删除它。
+            this.vwUserOrderTableAdapter.Fill(this.productDBDataSet.vwUserOrder);
+           
+           
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            var selectCustomerID = comboBox1.SelectedValue;
+            if (selectCustomerID==null)
+            {
+                return;
+            }
+            string cmd = "select * from vwUserOrder where CustomerID=@CustomerID";
+            var dt= SQLHelper.ExecuteDatatable(cmd, new SqlParameter("@CustomerID", selectCustomerID));
+            dataGridView1.DataSource = dt.DefaultView;
         }
     }
 }
---
+++ PudongTraining/PudongTraining/Utility/SQLHelper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace PudongTraining.Utility
+{
+    public class SQLHelper
+    {
+        private static readonly string ConnString = "Data Source=.;Initial Catalog=ProductDB;Integrated Security=True";
+        public static int ExcuteNoQuery(string cmd,params SqlParameter[] pars){
+            using (SqlConnection conn=new SqlConnection(ConnString))
+            {
+                conn.Open();
+                SqlCommand scmd = new SqlCommand(cmd, conn);
+                scmd.Parameters.AddRange(pars);
+                return scmd.ExecuteNonQuery();
+            
+            }
+
+        }
+
+        public static DataTable ExecuteDatatable(string cmd, params SqlParameter[] pars)
+        {
+            using (SqlConnection conn=new SqlConnection(ConnString))
+            {
+                conn.Open();
+
+                SqlCommand scmd = new SqlCommand(cmd, conn);
+                scmd.Parameters.AddRange(pars);
+
+                SqlDataAdapter sda = new SqlDataAdapter(scmd);
+                DataSet ds = new DataSet();
+                sda.Fill(ds);
+
+                return ds == null ? null : ds.Tables[0];
+                
+            }
+
+        }
+    }
+}
